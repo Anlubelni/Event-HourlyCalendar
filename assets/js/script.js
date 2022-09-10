dff16c3ea3e3b10ab800fa57b7bbebc04d6fcd61
Anlubelni <anlubelni@gmail.com>
2022-09-10 10:56:57 -0700

    DONE!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 
+
+//Code starts with what day it is at the moment
+
 function updateTime(){
     $("#twoDay").text(moment().format("dddd, MMMM Do"));
 }
@@ -8,21 +11,26 @@ function updateTime(){
 hourUpdater();
 function hourUpdater() {
 
-    var currentHour = moment().hours();
+// Portion of the code that identifies the hour of the day and blocks hours that have passed with the "if/else" conditional statements
+
+var currentHour = moment().hours();
     $(".time-block").each(function() {
         var blockHour = parseInt($(this).attr("id").split("-")[1]);
 
-        if (blockHour < currentHour) {
+        if (blockHour < currentHour) { 
             $(this).addClass("past");
-        
+        // do not want to show past hours
+
         } else if (blockHour === currentHour) {
             $(this).removeClass("past");
             $(this).addClass("present");
+        //marking current and past hours with the appropriate names "past"
+        
         } else {
             $(this).removeClass("past");
             $(this).removeClass("present");
             $(this).addClass("future");
-
+        //Otherwise it should display all three evenly
         }
 
     });
@@ -31,9 +39,11 @@ function hourUpdater() {
 
 
 
+
 $(document).ready(function() {
 
     $(".saveBtn").on("click", function() {
+// Adding functionality to the save button in order to keep information on screen
 
         var value = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
@@ -44,7 +54,7 @@ $(document).ready(function() {
 })
 
 
-
+//Saving all 9-5 hours
 $("#hour-9 .description").val(localStorage.getItem("hour-9"));
 $("#hour-10 .description").val(localStorage.getItem("hour-10"));
 $("#hour-11 .description").val(localStorage.getItem("hour-11"));
